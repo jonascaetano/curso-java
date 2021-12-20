2c04f555accd99111f5529954818746ef383f5c7
Jonas Caetano <jonascaettano@gmail.com>
2021-12-20 01:57:36 -0300

    commit added

--- src/cursoJava/Fundamentos.java
+++ src/cursoJava/Fundamentos.java
@@ -45,10 +45,47 @@ public class Fundamentos {
 		System.out.println("Estruturas de controle condicional");
 		System.out.println();
 		System.out.println("Exemplo 1 - Uso do 'if'");
-		sexo = 'F';
 		if (sexo == 'M') {
-			System.out.println("Alistamento militar obrigatório.");
+			System.out.println("Alistamento militar obrigatório");
 		}
+		System.out.println();
+		System.out.println("Exemplo 2 - Uso do 'if - else'");
+		idade = 17;
+		if (idade < 18) {
+			System.out.println("Menor de idade.");
+		} else {
+			System.out.println("Maior de idade");
+		}
+		System.out.println();
+		System.out.println("Exemplo 3 - Uso do 'else - if'");
+		if (idade < 16) {
+			System.out.println("PROIBIDO VOTAR");
+		} else if (idade >= 18 && idade <=70) {
+			System.out.println("VOTO OBRGATÓRIO");			
+		} else {
+			System.out.println("VOTO FACULTATIVO");
+		}
+		System.out.println();
+		System.out.println("Exemplo 4 - Uso do 'switch case'");
+		System.out.println("1. Cadastro de clientes");
+		System.out.println("2. Cadastro de usuários");
+		System.out.println("3. Relatórios");
+		int opcao = 3;
+		switch (opcao) {
+		case 1:
+			System.out.println("CLIENTES");
+			break;
+		case 2:
+			System.out.println("USUÁRIOS");
+			break;
+		case 3:
+			System.out.println("RELATÓRIOS");
+			break;
+		default:
+			System.out.println("Opção inválida");
+			break;
+		}
+		
 		
 	}
 
